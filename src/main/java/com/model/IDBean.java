7f633a38598e709c6f0c09d3b8aeb6d3bbdacd7e
myominnhtetnet-sys <myominnhtet.net@gmail.com>
2026-08-18 11:51:14 +0630

    Create IDBean.java

---
+++ src/main/java/com/model/IDBean.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter@ToString
+public class IDBean {
+	
+	private int id;
+	private String idNumber;
+}
